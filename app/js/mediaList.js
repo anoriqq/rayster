f8cf1229548567448d9f3a5960ee920ed7ca6cf6
show0317sw <33250012+show0317sw@users.noreply.github.com>
2018-10-20 20:26:53 +0900

    :sparkles: 左右ボタンにホバーまたはクリックしたときリスト表示する機能を追加
    フォーカスメディアから前後3ファイルを表示するようにした

--- app/js/mediaList.js
+++ app/js/mediaList.js
@@ -30,3 +30,54 @@ function focusChange(focusMedia, nextMedia){
   nextMedia.classList.add('focusMedia');
   mediaTitle.innerText = decodeURIComponent(path.basename(nextMedia.src));
 }
+
+/* 左右ボタンを2s間ホバーしたときにchangeMediaView関数を実行 */
+let changeMediaViewTimeout = null;
+let unListViewTimeout = null;
+const mediaChangeButton = getElemClass('mediaChangeButton');
+for(let i=0;i<mediaChangeButton.length;i++){
+  mediaChangeButton[i].addEventListener('mouseenter', function(){
+    window.clearTimeout(unListViewTimeout);
+    changeMediaViewTimeout = window.setTimeout(changeMediaView, 500);
+  });
+  mediaChangeButton[i].addEventListener('mouseleave', function(){
+    window.clearTimeout(changeMediaViewTimeout);
+    unListViewTimeout = window.setTimeout(unListView, 500);
+  });
+  mediaChangeButton[i].addEventListener('click', function(){
+    window.clearTimeout(changeMediaViewTimeout);
+    window.clearTimeout(unListViewTimeout);
+    changeMediaView();
+  });
+}
+
+/* メディアリストを変更する関数 */
+let regexp = /.*focusMedia.*/;
+function changeMediaView(){
+  let changeStartNumber = 0;
+  for(let i=0;i<medias.length;i++){
+    if(regexp.test(medias[i].classList.value)){
+      changeStartNumber = i-3;
+      break;
+    }
+  }
+  for(let i=0;i<medias.length;i++){
+    if(i >= changeStartNumber && i < changeStartNumber+7){
+      medias[i].classList.add('listView');
+    }else{
+      medias[i].classList.remove('listView');
+    }
+  }
+}
+
+/* クラスから要素配列を取得する関数 */
+function getElemClass(className){
+  return document.getElementsByClassName(className);
+}
+
+/* メディアを通常のビューみ戻す関数 */
+function unListView(){
+  for(let i=0;i<medias.length;i++){
+    medias[i].classList.remove('listView');
+  }
+}
